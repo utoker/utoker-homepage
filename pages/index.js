e0d22d1229e7f82c0f2660f3775353735d68b73c
Umut Toker <utoker@gmail.com>
2022-05-30 10:23:02 -0400

    integrate with chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
+import { Box, Container, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-  return <div>Page</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack developer based in NY.
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Umut Toker
+          </Heading>
+          <p>Developer</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 export default Page
