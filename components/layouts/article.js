d47abaf7a4d84bb3dc4c2d1e942140bdf91b3d99
Umut Toker <utoker@gmail.com>
2022-06-02 07:11:13 -0400

    change title

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -9,7 +9,7 @@ const variants = {
 }
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - Takuya Matsuyama`
+  const t = `${title} - Umut Toker`
   return (
     <motion.article
       initial="hidden"
